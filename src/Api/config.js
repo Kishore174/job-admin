e465c033e788d8db48824426abd9fa2177b557b2
Kishore174 <skishore174@gmail.com>
2026-02-17 18:09:53 +0530

    Removed node_modules and cache from repo

--- src/Api/config.js
+++ src/Api/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_BASE_URL = "https://job-api-wheat.vercel.app/api";
-const API_BASE_URL = "http://localhost:4000/api";
+const API_BASE_URL = "https://job-api-wheat.vercel.app/api";
+// const API_BASE_URL = "http://localhost:4000/api";
 
 
 const axiosInstance = axios.create({
